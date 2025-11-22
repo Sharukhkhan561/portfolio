04b28e88c2fc0e04b6afd53fbfa7d44746e10699
Sharukh Khan <enetsharukh@gmail.com>
2025-11-23 00:07:15 +0530

    Project setup and navigation component done in portfolio app.

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import Navbar from "./components/Navbar"
+import Home from "./sections/Home"
+import About from "./sections/About"
+import Skills from "./sections/Skills"
+import Project from './sections/Project'
+import Experience from './sections/Experience'
+import Footer from './sections/Footer'
+import ParticlesBackground from './components/ParticlesBackground'
+import CustomCursor  from './components/CustomCursor'
+function App() {
+
+  return (
+    <div className="relative gradient text-white">
+    <CustomCursor/>
+    <ParticlesBackground/>
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Project/>
+      <Experience/>
+      <Footer/> 
+    </div>
+  )
+}
+
+export default App
---
+++ src/sections/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-full h-screen'>
+      ABout
+    </div>
+  )
+}
+
+export default About
