998d9c45cb81464605e0b73761b3abf49e65f859
Sharukh Khan <enetsharukh@gmail.com>
2025-11-23 19:49:23 +0530

    Adding Navbar component .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import OverlayMenu from './OverlayMenu';
+import Logo from '../assets/Logo.png'
+import { FiMenu } from "react-icons/fi";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen]= useState(false);
+  const [visible, setVisible]= useState(true);
   return (
-    <div>Navbar</div>
+    <>
+
+    <nav className={`fixed top-0 left-0 w-full flex items-center justify-between px-6 py-4 z-50 transition-transform duration-300 ${visible? 'translate-y-0':'translate-y-full'}`}>
+    <div className='flex items-center space-x-2'>
+      <img src={Logo} className='w-8 h-8' alt="" />
+      <div className='text-2xl font-bold text-white hidden sm:block'>Sharukh</div>
+    </div>
+    <div className='block lg:absolute lg:left-1/2 lg:transform lg:translate-x-1/2'>
+      <button onClick={()=>setMenuOpen(true)} className='text-3xl text-white focus:outline-none cursor-pointer' aria-label='open Menu'><FiMenu/></button>
+    </div>
+    <div className='hidden lg:block'>
+      <a href="#contact" className='bg-gradient-to-r from-pink-500 to-blue-500 text-white  px-5  py-2 rounded-full font-medium
+      shadow-lg  hover:capacity-90 transition-opacity duration-300'>React Out</a>
+    </div>
+    </nav>
+      <OverlayMenu isOpen = {menuOpen} onClose={()=>setMenuOpen(false)}/>
+    </>
   )
 }
 
