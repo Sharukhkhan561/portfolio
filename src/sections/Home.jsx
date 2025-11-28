32d15e9450151ee39b186bbb914d583abcaa896d
Sharukh Khan <enetsharukh@gmail.com>
2025-11-28 19:48:25 +0530

    adding animation into home component.

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect, useMemo } from 'react'
 import ParticlesBackground from '../components/ParticlesBackground'
+import {motion} from 'framer-motion'
+import { useState } from 'react'
 
 const Home = () => {
+  const roles= useMemo(()=>["Frontend Developer", "Software Developer"],[])
+  const [index, setIndex]= useState(0);
+  const [subIndex, setSubIndex]= useState(0);
+  const [deleting, setDeleting]= useState(false);
+
+  useEffect(()=>{
+const current =roles[index];
+const timeout = setTimeout(()=>{
+  if(!deleting && subIndex < current.length) setSubIndex( v => v+1);
+  else if(!deleting && subIndex === current.length) setTimeout(()=> setDeleting(true),1500);
+  else if(deleting && subIndex > 0) setSubIndex(v => v - 1);
+  else if(deleting && subIndex === 0) {setDeleting(false); setIndex(p => (p + 1) % roles.length)}
+} , deleting ? 40 : 60)
+return () => clearTimeout(timeout);
+  },[subIndex, index, deleting, roles])
   return (
     <section id='home' className='w-full h-screen relative bg-black overflow-hidden'>
     <ParticlesBackground/>
@@ -21,7 +38,36 @@ const Home = () => {
 
       </div>
     </div>
-    <div className='relative z-10 h-full w-full max-w-7xl mx:auto px-4 grid grid-cols-1 lg:grid-cols-2'>
+    <div className='relative z-10 h-full w-full max-w-7xl mx-auto px-4 grid grid-cols-1 lg:grid-cols-2'>
+    <div className='flex flex-col justify-center h-full text-center lg:text-left relative'>
+       <div className='w-full lg:pr-24 mx-auto max-w-[48rem]'>
+           <motion.div className='mb-3 text-xl sm:text-2xl md:text-3xl lg:text-4xl font-semibold text-white tracking-wide min-h-[1.6em]'
+           initial={{opacity:0, y:12}}
+           animate={{opacity:1, y:0}}
+           transition={{duration:0.6}}>
+                <span>
+                  {roles[index].substring(0, subIndex)}
+                </span>
+                <span className='inline-block w-[2px] ml-1 bg-white animate-pulse align-middle'
+                style={{height: "1em"}}>
+
+                </span>
+           </motion.div>
+           <motion.h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-transparent bg-clip-text
+           bg-gradient-to-r from-[#1cd8d2] via-[#00bf8f] to-[#302b63] drop-shadow-lg'
+           initial={{opacity:0, y:40}}
+           animate={{opacity:1, y:0}}
+           transition={{duration:1}}
+           >
+           Hello, I am
+           <br/>
+           <span className='text-white font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>
+            Sharukh Khan
+           </span>
+           </motion.h1>
+       </div>
+
+    </div>
 
     </div>
     </section>
